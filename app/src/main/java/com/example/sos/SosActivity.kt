daabb57fc20fae9f10ee6cd78a01e78477602dcd
Manikandans1 <manimp377@gmail.com>
2024-02-29 17:19:10 +0530

    Design and backend work complete

--- app/src/main/java/com/example/sos/SosActivity.kt
+++ app/src/main/java/com/example/sos/SosActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sos
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.sos.databinding.ActivitySosBinding
 
 class SosActivity : AppCompatActivity() {
@@ -14,8 +16,16 @@ class SosActivity : AppCompatActivity() {
         setContentView(view)
 
         mainSosBinding.button3.setOnClickListener {
-            val intent = Intent(this@SosActivity,AlterActivity::class.java)
-            startActivity(intent)
+            var alter = AlertDialog.Builder(this)
+            alter.setIcon(R.drawable.baseline_warning_amber_24)
+            alter.setTitle("Alter Message")
+            alter.setMessage("Emergency Alter Message Sent")
+            alter.setCancelable(false)
+            alter.setNegativeButton("Yes", DialogInterface.OnClickListener { dialogInterface, i ->
+                dialogInterface.cancel()
+            })
+            alter.create().show()
         }
     }
 }
+
